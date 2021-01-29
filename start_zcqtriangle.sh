cae2d4857c3eafe0d731cc674af23e24d3291e8c
Aragorn1026 <294241960@qq.com>
2021-01-29 14:05:46 +0800

    Update start_zcqtriangle.sh

--- start_zcqtriangle.sh
+++ start_zcqtriangle.sh
@@ -2,4 +2,4 @@
 
 source /etc/profile
 
-java -javaagent:/nsf-agent-v2.6.6.jar=zcqtriangle-v2 -Dnsf.log.level=debug -jar zcqtriangle-v2.jar
+java -javaagent:/nsf-agent-v2.6.6.jar=noname -Dnsf.log.level=debug -jar zcqtriangle-v3.jar
