598b3467c4e0b8b749bc467e1e8033b332b255a2
Aragorn1026 <294241960@qq.com>
2021-02-02 16:33:42 +0800

    Update start_zcqtriangle.sh

--- start_zcqtriangle.sh
+++ start_zcqtriangle.sh
@@ -2,4 +2,5 @@
 
 source /etc/profile
 
-java -javaagent:/nsf-agent-v2.6.6.jar=zcqtriangle-v3 -Dnsf.log.level=debug -jar zcqtriangle-v3.jar
+# java -javaagent:/nsf-agent-v2.6.6.jar=zcqtriangle-vapm1 -Dnsf.log.level=debug -jar zcqtriangle-vapm1.jar
+java -javaagent:/napm-java-agent/napm-java-rewriter.jar=conf=napm-agent.properties -Dnsf.log.level=debug -jar zcqtriangle-vapm1.jar
